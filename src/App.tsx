6d399b00d7e5b51759584fac632da3899ccd9612
Islam <islam.music@scv.si>
2024-03-15 10:10:19 +0100

    pregled filmov

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,38 @@ import Footer from "./components/Footer.tsx";
 import Header from "./components/Header.tsx";
 import Welcome from "./components/Welcome.tsx";
 import Card from "./components/Card.tsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+    const apiKey = "4714c72a";
+    const search = "Dune";
+    const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${search}`;
 
-  return (
-      <>
-          <Header />
-          <main>
-              <Welcome />
-              <Card />
-          </main>
-          <Footer />
-      </>
-  )
+    const [movies,setMovies] = useState([]);
+    const loadMovies = async () => {
+        const response = await axios.get(apiUrl);
+        setMovies(response.data.Search);
+        //console.log(response.data);
+    }
+
+    useEffect(()=>{
+        loadMovies();
+    },[]);
+
+  return <>
+      <Header />
+      <main>
+          <Welcome />
+          {
+              movies.map((movie,i) => {
+                //console.log(movie);
+                return <Card key={i} movie={movie} />;
+                })
+          }
+      </main>
+      <Footer />
+  </>
 }
 
 export default App
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,31 +1,14 @@
-import {useState} from "react";
-import * as axios from "axios";
-
-const Card = () => {
-    const [movie, setMovie] = useState('');
-
-    const getMovie = () => {
-        const response = axios.get("http://www.omdbapi.com/?apikey=4714c72a&s=Dune: Part Two");
-    }
-
+const Card = ({movie}) => {
   return (
       <>
           <div className="album py-5 bg-body-tertiary">
               <div className="container">
-
                   <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                       <div className="col">
                           <div className="card shadow-sm">
-                              <svg className="bd-placeholder-img card-img-top" width="100%" height="225"
-                                   xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-                                   preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                                  <rect width="100%" height="100%" fill="#55595c"/>
-                                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                              </svg>
+                              <img src={movie.Poster} alt="Thumbnail" width="100%" />
                               <div className="card-body">
-                                  <p className="card-text">This is a wider card with supporting text below as a
-                                      natural lead-in to additional content. This content is a little bit
-                                      longer.</p>
+                                  <p className="card-text">{movie.Title}</p>
                                   <div className="d-flex justify-content-between align-items-center">
                                       <div className="btn-group">
                                           <button type="button" className="btn btn-sm btn-outline-secondary">View
